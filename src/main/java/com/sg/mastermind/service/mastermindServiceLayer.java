1e3aed5c41c01d4cfb3423951079ed532d4e404a
Darryl-A <darryl.prof7@gmail.com>
2023-03-06 15:00:05 -0500

    UpdateMessageDarryl
    Testing a change.

--- src/main/java/com/sg/mastermind/service/mastermindServiceLayer.java
+++ src/main/java/com/sg/mastermind/service/mastermindServiceLayer.java
@@ -16,155 +16,152 @@ import org.springframework.stereotype.Service;
  * @author darrylanthony
  */
 @Component
-public class mastermindServiceLayer{
-    //Import the DI for both Daos
+public class mastermindServiceLayer {
+    // Import the DI for both Daos
     private final gameDao GD;
     private final roundDao RD;
-    
-    public mastermindServiceLayer(gameDao GD, roundDao RD){
+
+    public mastermindServiceLayer(gameDao GD, roundDao RD) {
         this.GD = GD;
         this.RD = RD;
     }
-    
-    //Create a new game
-    public int createNewGame(){
-        //Start a game
+
+    // Create a new game
+    public int createNewGame() {
+        // Start a game
         Game newGame = new Game();
-        
-        //Generates an answer
+
+        // Generates an answer
         String answer = generateAnswer();
         newGame.setAnswer(answer);
         newGame.setGameStatus(true);
-        
-        //Add the game
+
+        // Add the game
         newGame = GD.addGame(newGame);
-        
+
         int gameID = newGame.getGameID();
         return gameID;
-        
+
     }
-    
-    //Generates an answer when the game starts
-    public String generateAnswer(){
-        
-        //Create a random number generator
+
+    // Generates an answer when the game starts
+    public String generateAnswer() {
+
+        // Create a random number generator
         Random rand = new Random();
-        
-        //Create an array to store 4 numbers
+
+        // Create an array to store 4 numbers
         int[] digits = new int[4];
-        
-        //Add the first digit to array
+
+        // Add the first digit to array
         int firstDigit = rand.nextInt(10);
         digits[0] = firstDigit;
-        
-        //Add second as long as it's not the same others
+
+        // Add second as long as it's not the same others
         int secondDigit = rand.nextInt(10);
-        while(secondDigit == firstDigit)
+        while (secondDigit == firstDigit)
             secondDigit = rand.nextInt(10);
         digits[1] = secondDigit;
-        
-        //Add third as long as it's not the same others
+
+        // Add third as long as it's not the same others
         int thirdDigit = rand.nextInt(10);
-        while(thirdDigit == secondDigit || thirdDigit == firstDigit)
+        while (thirdDigit == secondDigit || thirdDigit == firstDigit)
             thirdDigit = rand.nextInt(10);
         digits[2] = thirdDigit;
-        
-        //Add fourth as long as it's not the same others
+
+        // Add fourth as long as it's not the same others
         int fourthDigit = rand.nextInt(10);
-        while(fourthDigit == thirdDigit || fourthDigit == secondDigit ||fourthDigit == firstDigit)
+        while (fourthDigit == thirdDigit || fourthDigit == secondDigit || fourthDigit == firstDigit)
             fourthDigit = rand.nextInt(10);
         digits[3] = fourthDigit;
-        
-        //Convert the array of integers into a string
+
+        // Convert the array of integers into a string
         String gameNumber = String.join("", IntStream.of(digits).mapToObj(String::valueOf).toArray(String[]::new));
         return gameNumber;
     }
-    
-    public Round guess (Round round){
-        
-        //Get the round id
+
+    public Round guess(Round round) {
+
+        // Get the round id
         int roundID = round.getGameID();
-        
-        //Get the game being played
+
+        // Get the game being played
         Game currentGame = GD.getGameById(roundID);
-        
-        //Get the correct answer
+
+        // Get the correct answer
         String correctAnswer = currentGame.getAnswer();
-        
-        //Get the user guess
+
+        // Get the user guess
         String userGuess = round.getGuess();
-        
-        //Use the formula to see if answer is correct
+
+        // Use the formula to see if answer is correct
         String roundResult = calculateResult(correctAnswer, userGuess);
-        
-        //Update the result
+
+        // Update the result
         round.setResult(roundResult);
-        
-        //If guess is correct,
-        if(userGuess.equals(correctAnswer)){
+
+        // If guess is correct,
+        if (userGuess.equals(correctAnswer)) {
             currentGame.setGameStatus(false);
             GD.updateGame(currentGame);
         }
-        
-        //Finally, add the round.
+
+        // Finally, add the round.
         return RD.addRound(round);
     }
-    
-    
-    //Calculate the result of the round
-    public static String calculateResult(String correctAnswer, String userGuess){
+
+    // Calculate the result of the round
+    public static String calculateResult(String correctAnswer, String userGuess) {
         char[] first = correctAnswer.toCharArray();
         char[] second = userGuess.toCharArray();
-        
+
         int e = 0;
         int p = 0;
-        
-        for(int i = 0; i<first.length; i++){
+
+        for (int i = 0; i < first.length; i++) {
             char str = second[i];
             String pop = Character.toString(str);
-            if (correctAnswer.contains(pop)){
-                if(first[i] == second[i]){
+            if (correctAnswer.contains(pop)) {
+                if (first[i] == second[i]) {
                     e++;
-                }
-                else
+                } else
                     p++;
             }
         }
         String result = "e:" + e + ":p:" + p;
         return result;
     }
-    
-    //Returns a list of all games
-    public List<Game> getAllGames(){
-        //Store all the names in a list
+
+    // Returns a list of all games
+    public List<Game> getAllGames() {
+        // Store all the names in a list
         List<Game> allGames = GD.getAllGames();
-        
-        //Make sure games in progess are not displayed
-        for (Game g: allGames){
-            if(g.getGameStatus() == true){
+
+        // Make sure games in progess are not displayed
+        for (Game g : allGames) {
+            if (g.getGameStatus() == true) {
                 g.setAnswer("Nice try buddy.");
             }
         }
         return allGames;
     }
-    
-    
-    //Returns a game based on ID
-    public Game getGameByID(int gameID){
-        //Retrieve the game
+
+    // Returns a game based on ID
+    public Game getGameByID(int gameID) {
+        // Retrieve the game
         Game currentGame = GD.getGameById(gameID);
-        
-        //If in progress, hide answer
-        if(currentGame.getGameStatus() == true){
-                currentGame.setAnswer("Nice try buddy.");
-            }
+
+        // If in progress, hide answer
+        if (currentGame.getGameStatus() == true) {
+            currentGame.setAnswer("Wrong Answer Bro.");
+        }
         return currentGame;
     }
-    
-    //Gets all the rounds of a specific game
-    public List<Round> getRoundsByGameID(int gameID){
-        
-        //Store all the rounds
+
+    // Gets all the rounds of a specific game
+    public List<Round> getRoundsByGameID(int gameID) {
+
+        // Store all the rounds
         List<Round> allRounds = RD.getAllRoundsByGameID(gameID);
         return allRounds;
     }
